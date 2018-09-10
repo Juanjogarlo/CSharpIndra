f00008621882abc483a9b1e405bb4eb64c8bad21
Juanjo C# <juanjogarlo@gmail.com>
2018-09-10 13:03:08 +0200

    Subida del proyecto C# de Microsoft

---
+++ AppBonoloto/AppBonoloto/Bonoloto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppBonoloto
+{
+    class Bonoloto
+    {
+        public int numero;
+        public int fecha;
+    }
+}
